c138967fbba1dbd8659e1918cb2d021dcb3a3e3c
Venkat Padala <152841070+Venkat3339@users.noreply.github.com>
2025-12-01 12:57:32 +0530

    Remove

--- PythonPractise/Question16.py
+++
@@ -1,7 +0,0 @@
-# Create a program that prints numbers between 1 and 100 that are divisible by either 3 or 5 but not both.
-
-print("numbers between 1 and 100 that are divisible by either 3 or 5 but not both: \n")
-# Loop to print numbers between 1 and 100 that are divisible by either 3 or 5 but not both
-for i in range(1,101):
-    if(i%3 == 0 or i%5 == 0) and not(i%3 == 0 and i%5 == 0):
-        print(i)
